5366cf11b63f7ce69c0fb6b91b466118269df373
Cyberproton <cyberprotonAtMailbox@gmail.com>
2021-05-11 21:12:16 +0700

    init

---
+++ app/src/main/java/hcmut/team15/emergencysupport/MainActivity.kt
@@ -0,0 +1,11 @@
+package hcmut.team15.emergencysupport
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
