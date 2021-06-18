f2f58cedc9180af8805515ae86a30c975f98983c
Quan-dang0405 <quandang0405@gmail.com>
2021-06-18 19:15:38 +0700

    google map

--- app/src/main/java/hcmut/team15/emergencysupport/profile/ProfileActivity.java
+++ app/src/main/java/hcmut/team15/emergencysupport/profile/ProfileActivity.java
@@ -1,19 +1,36 @@
 package hcmut.team15.emergencysupport.profile;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
+import hcmut.team15.emergencysupport.MainActivity;
 import hcmut.team15.emergencysupport.MainApplication;
 import hcmut.team15.emergencysupport.MenuActivity;
 import hcmut.team15.emergencysupport.R;
@@ -24,10 +41,45 @@ import retrofit2.Response;
 
 public class ProfileActivity extends AppCompatActivity {
     private ProfileInterface profileInterface;
+
+    SupportMapFragment supportMapFragment;
+    double lat,lng;
+    String addressLine;
+    TextView userLocation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_activity);
+
+        supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.userMap);
+        userLocation = findViewById(R.id.userLocation);
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull @NotNull GoogleMap googleMap) {
+                try {
+                    Geocoder geocoder = new Geocoder(ProfileActivity.this
+                            , Locale.getDefault());
+
+                    List<Address> addresses = geocoder.getFromLocation(
+                            60, 60, 1
+                    );
+
+                    lat = addresses.get(0).getLatitude();
+                    lng = addresses.get(0).getLongitude();
+                    addressLine = addresses.get(0).getAddressLine(0);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                userLocation.setText("Tôi đang đứng ở: " + addressLine);
+                LatLng latLng = new LatLng(lat, lng);
+                MarkerOptions options = new MarkerOptions().position(latLng).title("I am there");
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                googleMap.addMarker(options);
+            }
+        });
 
         profileInterface = MainApplication.getInstance().getRetrofit().create(ProfileInterface.class);
         Button apply_btn = findViewById(R.id.apply_btn);
@@ -79,6 +131,7 @@ public class ProfileActivity extends AppCompatActivity {
         EditText dateOfBirth = findViewById(R.id.editTextBirthDay);
         EditText bloodType = findViewById(R.id.editTextBloodType);
         EditText allergy = findViewById(R.id.editTextAllergy);
+
         profileInterface.getProfile(TokenVar.AccessToken).enqueue(new Callback<ProfileResponse>() {
             @Override
             public void onResponse(Call<ProfileResponse> call, Response<ProfileResponse> response) {
@@ -98,5 +151,6 @@ public class ProfileActivity extends AppCompatActivity {
                 Log.d("error", "error");
             }
         });
+
     }
 }
